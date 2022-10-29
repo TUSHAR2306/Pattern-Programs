6a428fac87f40d4aa5a02dfc91aa334692a88079
Tushar <tusharsharma0602@gmail.com>
2022-10-29 19:20:26 +0530

    Deleted all files which are already present in master and added new files prog22.cpp and prog23.cpp

--- prog23.cpp
+++ prog23.cpp
@@ -3,12 +3,16 @@ using namespace std;
 
 int main(){
     int n;
+    char k;
     cin >> n;
 
     for(int i=1;i<=n;i++){
-        for(int j=0;j<=2*n;j++){
-            if(j>=n-i+1 && j<=2*n-i+1){
-                cout << "*";
+        k='1';
+        for(int j=1;j<=2*n-1;j++){
+            if(j>=n-i+1 && j<=n+i-1){
+                if(j==n+1)k='A';
+                cout << k;
+                k++;
             }
             else{
                 cout << " ";
@@ -20,12 +24,10 @@ int main(){
 
 /*
 
-     ******
-    ******
-   ******
-  ******
- ******
-
+    1   
+  12A
+ 123AB
+1234ABC
 
 
 
